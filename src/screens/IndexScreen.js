8a8c6f5ae965a28ea0afad74d1cefb911257c211
astronaut99 <rakshitkumar90@gmail.com>
2021-04-05 00:26:08 +0530

    useContext added

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import BlogContext from "./context/BlogContext";
+
+const IndexScreen = () => {
+  const blogPosts = useContext(BlogContext);
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
+};
+
+export default IndexScreen;
